e0ecee3a7949d9c29588815afb392cf35c48eb26
Hyeonyong16 <Hyeonyong16@users.noreply.github.com>
2019-10-06 02:12:24 +0900

    테트리스_바닥도착시 정지
    바닥닿을시 정지
    옆에 보이는 다음블럭은 움직이지 않음
    
    ---------
    해야할거
    다음블럭 가져오기
    블럭끼리 충돌설정
    
    생각나면 추가

--- Screen/Screen/Utils.h
+++ Screen/Screen/Utils.h
@@ -1,25 +1,110 @@
 #ifndef UTILS_H_
 #define UTILS_H_
-
+#include <cstdio>
 #include <Windows.h>
+
+using namespace std;
 
 struct Position {
 	int x;
 	int y;
 	Position(int x = 0, int y = 0) : x(x), y(y) {}
+
+	Position(const Position& other) 
+		: Position(other.x, other.y) {}
 };
 
-class Borland {
+class Input {
+	static INPUT_RECORD InputRecord[128];
+	static DWORD Events;
+	static bool evaluated;
+	static bool gotMouseEvent;
+	static bool gotKeyEvent;
+	static Position mousePosition;
+	static WORD vKeyCode;
+
+	static void GetEvent()
+	{
+		evaluated = true;
+		DWORD numEvents = 0;
+
+		GetNumberOfConsoleInputEvents(GetStdHandle(STD_INPUT_HANDLE), &numEvents);
+		if (!numEvents) return;
+
+		ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), InputRecord, numEvents, &Events);
+		for (int i = 0; i < Events; i++) {
+			if (InputRecord[i].EventType == MOUSE_EVENT) {
+				if (InputRecord[i].Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
+					COORD coord;
+					coord.X = InputRecord[i].Event.MouseEvent.dwMousePosition.X;
+					coord.Y = InputRecord[i].Event.MouseEvent.dwMousePosition.Y;
+					SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+					mousePosition.x = InputRecord[i].Event.MouseEvent.dwMousePosition.X;
+					mousePosition.y = InputRecord[i].Event.MouseEvent.dwMousePosition.Y;
+					gotMouseEvent = true;
+				}
+			}
+			else if (InputRecord[i].EventType == KEY_EVENT) {
+				if (InputRecord[i].Event.KeyEvent.bKeyDown) {
+					vKeyCode = InputRecord[i].Event.KeyEvent.wVirtualKeyCode;
+					gotKeyEvent = true;
+				}
+			}
+		}
+	}
 
 public:
-	static void initialize()
+
+	static void EndOfFrame()
 	{
+		evaluated = false;
+		gotMouseEvent = false;
+		gotKeyEvent = false;
+	}
+
+	static void Initialize()
+	{
+
 		CONSOLE_CURSOR_INFO cci;
 		cci.dwSize = 25;
 		cci.bVisible = FALSE;
 		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cci);
-		SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT);
+		SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT );
+
+		EndOfFrame();
 	}
+	static bool GetMouseEvent(Position& pos) {
+		if (evaluated == false) GetEvent();
+
+		if (gotMouseEvent == true) {
+			pos = mousePosition;
+			return true;
+		}
+		return false;}
+
+	static bool GetKeyEvent(WORD& keyCode) {
+		if (evaluated == false) GetEvent();
+
+		if (gotKeyEvent == true) {
+			keyCode = vKeyCode;
+			return true;
+		}
+		return false;
+	}
+};
+INPUT_RECORD Input::InputRecord[128];
+DWORD Input::Events;
+
+bool Input::evaluated = false;
+bool Input::gotMouseEvent = false;
+bool Input::gotKeyEvent = false;
+Position Input::mousePosition{ -1, -1 };
+WORD Input::vKeyCode{ 0 };
+
+class Borland {
+
+public:
+	
 
 	static int wherex()
 	{
@@ -47,5 +132,64 @@ public:
 		gotoxy( pos.x, pos.y);
 	}
 };
+
+class Screen {
+	int width;
+	int height;
+	char* canvas;
+
+	static Screen* instance;
+	Screen(int width = 80, int height = 25)
+		: width(width), height(height),
+		canvas(new char[(width + 1)*height])
+
+	{
+		Input::Initialize();
+	}
+public:
+	static Screen& getInstance() {
+		if (instance == nullptr) {
+			instance = new Screen();
+		}
+		return *instance;
+	}
+
+	~Screen() {
+		if (instance) {
+			delete[] canvas;
+			instance = nullptr;
+		}
+	}
+
+	void draw(const char* shape, int w, int h, const Position& pos)
+	{
+		if (!shape) return;
+		for (int i = 0; i < h; i++)
+		{
+			strncpy(&canvas[pos.x + (pos.y + i)*(width + 1)], &shape[i*w], w);
+		}
+	}
+
+	void render()
+	{
+		for (int i = 0; i < height; i++)
+			canvas[width + i * (width + 1)] = '\n';
+		canvas[width + (height - 1) * (width + 1)] = '\0';
+		Borland::gotoxy(0, 0);
+		cout << canvas;
+	}
+
+	void clear()
+	{
+		memset(canvas, ' ', (width + 1)*height);
+		canvas[width + (height - 1)*(width + 1)] = '\0';
+	}
+
+
+	int getHeight() const { return height;  }
+	int getWidth() const { return width;  }
+};
+
+Screen* Screen::instance = nullptr;
 
 #endif 
--- Screen/Screen/main.cpp
+++ Screen/Screen/main.cpp
@@ -2,268 +2,201 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 #include <conio.h>
 #include <Windows.h>
+#include <vector>
+#include <string>
+#include <ctime>
 #include "Utils.h"
 
 using namespace std;
 
-class Screen {
-	int width;
-	int height;
-	char* canvas;
-
-	static Screen* instance;
-	Screen(int width = 70, int height = 25) 
-		: width(width), height(height),
-		canvas(new char[(width+1)*height])
-
-	{
-		Borland::initialize();
-	}
-public:
-	static Screen& getInstance() {
-		if (instance == nullptr) {
-			instance = new Screen();
-		}
-		return *instance;
-	}
-
-	~Screen() {
-		if (instance) {
-			delete[] canvas;
-			instance = nullptr;
-		}
-	}
-
-	void draw(const char* shape, int w, int h, const Position& pos)
-	{
-		if (!shape) return;
-		for (int i = 0; i < h; i++)
-		{
-			strncpy(&canvas[pos.x + (pos.y+i)*(/*width+1*/w)], &shape[i*w], w);
-		}
-	}
-
-	void render()
-	{
-		Borland::gotoxy(0, 0);
-		cout << canvas;
-	}
-
-	void clear()
-	{
-		memset(canvas, ' ', (width + 1)*height);
-		canvas[width + (height-1)*(width+1)] = '\0';
-	}
-
-};
-
-Screen* Screen::instance = nullptr;
-
 class GameObject {
-	char* shape;
-	int width;
-	int height;
-	Position pos;
-	Screen& screen;
+	char*		shape;
+	int			width;
+	int			height;
+	Position	pos;
+	Screen&		screen;
+
+	bool			 isMove;		//움직일 블럭인지 아닌지 체크
+
+	vector<GameObject *> children;
+
 
 public:
-	GameObject(const char* shape, int width, int height) 
-		: height(height), width(width), shape(nullptr), pos(0, 0),
-		screen(Screen::getInstance())
-	{
+	GameObject(const char* shape, int width, int height, const Position& pos = Position{ 0, 0 })
+		: height(height), width(width),
+		shape(nullptr), pos(pos), isMove(true),
+		screen(Screen::getInstance()) {
 		if (!shape || strlen(shape) == 0 || width == 0 || height == 0)
 		{
 			this->shape = new char[1];
 			this->shape[0] = 'x';
 			width = 1;
 			height = 1;
-		} else {
-			this->shape = new char[(width+1)*height];
-			strncpy(this->shape, shape, (width+1)*height);
+		}
+		else {
+			this->shape = new char[width*height];
+			strncpy(this->shape, shape, width*height);
 		}
 		this->width = width;
 		this->height = height;
 	}
+
 	virtual ~GameObject() {
 		if (shape) { delete[] shape; }
 		width = 0, height = 0;
 	}
 
-	void setMine() {
-		srand((unsigned)time(NULL));
+	void add(GameObject* child) {
+		if (!child) return;
+		children.push_back(child);
+	}
 
-		for (int i = 0; i < (width + 1) * height; i++) {
-				shape[i] = '0';
-				if (rand() % 10 == 0) 
-					shape[i] = '*';
+	void setShape(const char* shape) {
+		if (!shape) return;
+		strncpy(this->shape, shape, width*height);
+	}
+
+	void setPos(int x, int y) { this->pos.x = x; this->pos.y = y; }
+
+	Position& getPos() { return pos; }
+
+	bool& getIsMove() { return isMove; };
+
+	virtual void draw()
+	{
+		screen.draw(shape, width, height, pos);
+
+		for (auto child : children) child->draw();
+	}
+
+	void internalUpdate() {
+		update();
+		for (auto child : children)
+			child->update();
+	}
+
+	virtual void update()
+	{
+	}
+
+	vector<GameObject *>& getChildren() { return children; }
+};
+
+class Block : public GameObject {
+
+	vector<string> sprites;
+	int current;
+
+public:
+	Block(vector<string>& sprites, int w, int h,
+		const Position& pos = Position{ 0,0 })
+		: sprites(sprites), current(0),
+		GameObject(sprites[current].c_str(), w, h, pos) {}
+
+	void update() {
+		WORD keyCode;
+		if (getPos().y >= Screen::getInstance().getHeight() - 5) {
+			getIsMove() = false;
+			return;
+		}
+		if (getPos().x > 24) {
+			getIsMove() = false;
+			return;
 		}
 
-		for (int i = 0; i < height; i++) {
-			shape[(width + 1) * i + width] = '\n';
-		}
+		if (getIsMove() == true) {
+			if (Input::GetKeyEvent(keyCode)) {
+				switch (keyCode) {
+				case VK_RIGHT:
+					if (getPos().x >= 24) break;
+					getPos().x++;
+					break;
 
-		shape[(width + 1) * (height - 1) + width] = '\0';
+				case VK_LEFT:
+					if (getPos().x <= 0) break;
+					getPos().x--;
+					break;
 
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				int pos = (width + 1) * i + j;
-				if (shape[pos] != '*') {
-					int mineNum = 0;	//주변 지뢰숫자를 저장할 변수
-					int x, y, xMax, yMax;
-					//각 모서리일때 예외처리용 변수(상하좌우)
-					if (i == 0) y = 0;	
-					else y = -1;
-					if (j == 0) x = 0;
-					else x = -1;
-					if (i == height - 1) yMax = 1;
-					else yMax = 2;
-					if (j == width - 1) xMax = 1;
-					else xMax = 2;
-					//주변 8칸 확인해서 지뢰가 있을경우 mineNum 1씩 증가
-					//								  -1          0         1
-					//width * y + x + y		====-1 -width-2   -width-1   -width
-					//주변 8칸 정보			==== 0	  -1	      0		    1
-					//						==== 1   width     width+1   width+2
-					for (int h = y; h < yMax; h++) {
-						for (int w = x; w < xMax; w++) {
-							if (shape[pos + (width * h) + w + h] == '*') mineNum++;
-						}
-					}
+				case VK_UP:
+					current = (current + 1) % sprites.size();
+					setShape(sprites[current].c_str());
+					break;
 
-					shape[pos] = '0' + mineNum;
+				case VK_DOWN:
+					getPos().y = Screen::getInstance().getHeight() - 6;
+					break;
+
+				case 0x41: //'a'
+					current = (current + 1) % sprites.size();
+					setShape(sprites[current].c_str());
+					break;
+
+				case 0x44: //'d'
+					current = (current + 3) % sprites.size();
+					setShape(sprites[current].c_str());
+					break;
 				}
 			}
-		}
-	}
-
-	void setBoard() {
-		for (int i = 0; i < (width + 1) * height; i++) {
-			shape[i] = '+';
-		}
-
-		for (int i = 0; i < height; i++) {
-			shape[(width + 1) * i + width] = '\n';
-		}
-
-		shape[(width + 1) * (height - 1) + width] = '\0';
-	}
-
-	char* getShape() {
-		return shape;
-	}
-
-	int checkGameover(const char* mine) {
-		bool checkAll = true;
-		for (int i = 0; i < (width + 1) * height; i++) {
-			if (shape[i] == '*')
-				//지뢰가 있으면
-				return 1;
-			if (shape[i] == '+' && mine[i] != '*')
-				checkAll = false;
-		}
-
-		//다 찾았을시
-		if (checkAll == true)
-			return 2;
-
-		//지뢰가 없으면
-		return 0;
-	}
-
-	void draw() {
-		screen.draw(shape, width+1, height, pos);
-	}
-
-	virtual void update(Position& pos, char* mine, bool leftClick) {
-		if (leftClick == true) {
-			//width * pos.y + pos.x 배열에서 위치
-			if (mine[(width + 1) * pos.y + pos.x] == '*') {
-				shape[(width + 1) * pos.y + pos.x] = mine[(width + 1) * pos.y + pos.x];
-			}
-			else if (mine[(width + 1) * pos.y + pos.x] > '0' && mine[(width + 1) * pos.y + pos.x] < '9') {
-				shape[(width + 1) * pos.y + pos.x] = mine[(width + 1) * pos.y + pos.x];
-			}
-			else if (mine[(width + 1) * pos.y + pos.x] == '0') {
-				shape[(width + 1) * pos.y + pos.x] = mine[(width + 1) * pos.y + pos.x];
-				//width*(pos.y-1)+(pos.x-1)   width*(pos.y-1)+pox.x   width*(pos.y-1)+(pox.x+1)  
-				//width * pos.y + (pos.x-1)   width * pos.y + pos.x   width * pos.y + (pos.x+1)
-				//width*(pos.y+1)+(pos.x-1)   width*(pos.y-1)+pos.x   width*(pos.y+1)+(pox.x+1)
-				for (int i = -1; i < 2; i++) {
-					for (int j = -1; j < 2; j++) {
-						if (pos.y + i >= 0 && pos.y + i < height && pos.x + j >= 0 && pos.x + j < width) {
-							Position nextPos(pos.x + j, pos.y + i);
-							if (shape[(width + 1) * nextPos.y + nextPos.x] == '+')
-								update(nextPos, mine, true);
-						}
-					}
-				}
-			}
-		}
-
-		//깃발
-		if (leftClick == false) {
-			shape[(width + 1) * pos.y + pos.x] = '=';
+			getPos().y = getPos().y + 1;
 		}
 	}
 };
 
 int main()
 {
-	char mine[110];
-	char board[110];
-	int gameover = 0;
-	GameObject playerBoard(board, 10, 10);
-	GameObject mineBoard(mine, 10, 10);
-	Screen&	 screen = Screen::getInstance();
-	INPUT_RECORD InputRecord;
-	DWORD Events;
+	//ㄴ자 블럭
+	vector<string> sprites1{ "\x78\x78  \x78  \x78 ",
+		"   \x78\x78\x78  \x78",
+		" \x78  \x78 \x78\x78 ",
+		"   \x78  \x78\x78\x78"
+	};
+	//ㅁ자 블럭
+	vector<string> sprites2{ "   \x78\x78 \x78\x78 " };
+	//역 ㄴ자 블럭
+	vector<string> sprites3{ "     \x78\x78\x78\x78",
+	"\x78\x78  \x78  \x78 ",
+	"   \x78\x78\x78\x78  ",
+	"\x78  \x78  \x78\x78 "
+	};
+	//l자 블럭
+	vector<string> sprites4{ "\x78  \x78  \x78  ",
+	"      \x78\x78\x78"
+	};
 
-	playerBoard.setBoard();
-	mineBoard.setMine();
+	Screen&	 screen = Screen::getInstance();
+	vector<GameObject *> gameObjects;
+
+	srand(time(nullptr));
+
+	string mode{ "mode con cols="
+		+ to_string(screen.getWidth() + 4)
+		+ " lines=" + to_string(screen.getHeight() + 5) };
+	system(mode.c_str());
+	system("chcp 437");
+
+	auto parent = new Block{ sprites1, 3,3, Position{7, 0} };		//움직일 블럭
+	auto child = new Block{ sprites2, 3,3, Position{30, 3} };		//다음으로 보여줄 블럭
+
+	parent->add(child);
+	gameObjects.push_back(parent);
+
+	screen.clear(); screen.render();
 
 	while (true)
 	{
-		playerBoard.draw();
-		screen.render();	
-
-		printf("\n LeftClick: check, RightClick: flag");
-		gameover = playerBoard.checkGameover(mineBoard.getShape());
-		if (gameover == 1)
-		{
-			printf("\n===GameOver!===\n");
-			break;
-		}
-
-		else if (gameover == 2)
-		{
-			printf("\n===Clear!===\n");
-			break;
-		}
-
-		Sleep(30);
-		
-		ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &InputRecord, 1, &Events);
-		if (InputRecord.EventType == MOUSE_EVENT) {
-			if (InputRecord.Event.MouseEvent.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED) {
-				Position pos;
-				pos.x = InputRecord.Event.MouseEvent.dwMousePosition.X;
-				pos.y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
-
-				playerBoard.update(pos, mineBoard.getShape(), true);
-			}
-			if (InputRecord.Event.MouseEvent.dwButtonState & RIGHTMOST_BUTTON_PRESSED) {
-				Position pos;
-				pos.x = InputRecord.Event.MouseEvent.dwMousePosition.X;
-				pos.y = InputRecord.Event.MouseEvent.dwMousePosition.Y;
-
-				playerBoard.update(pos, mineBoard.getShape(), false);
-			}
-		}
 		screen.clear();
+		for (auto obj : gameObjects) obj->internalUpdate();
+
+		for (auto it = gameObjects.cbegin();
+			it != gameObjects.cend(); it++)
+			(*it)->draw();
+
+		screen.render();
+		Sleep(150);
+
+		Input::EndOfFrame();
 	}
 
 	return 0;
